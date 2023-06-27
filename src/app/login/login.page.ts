7089bc072b7bfa09f55a28c41a1d80a65d1fb244
SCatalanH <scatalanhermosilla@gmail.com>
2023-06-27 07:54:09 -0400

    V5.6  mit firebase fix

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from 'recruiterApp/node_modules/@angular/forms';
+import { Router } from 'recruiterApp/node_modules/@angular/router';
+import { AlertController, LoadingController } from 'recruiterApp/node_modules/@ionic/angular';
+import { AuthService } from '../services/auth.service';
+import { FormGroup } from 'recruiterApp/node_modules/@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  credentials: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private loadingController: LoadingController,
+    private alertController: AlertController,
+    private authService: AuthService,
+    private router: Router
+
+  ) { }
+
+  get email() {
+    return this.credentials.get('email');
+  }
+
+  get password(){
+    return this.credentials.get('password');
+  }
 
   ngOnInit() {
+    this.credentials = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['',[Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  async register() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+
+    const user = await this.authService.register(this.credentials.value);
+    await loading.dismiss();
+
+    if (user){
+      this.router.navigateByUrl('/folder', {replaceUrl: true});
+    } else {
+      this.showAlert('Registration Failed', 'Please try Again!');
+    }
+  }
+  
+  async login() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+
+    const user = await this.authService.login(this.credentials.value);
+    await loading.dismiss();
+
+    if (user){
+      this.router.navigateByUrl('/folder', {replaceUrl: true});
+    } else {
+      this.showAlert('Login Failed', 'Please try Again!');
+    }
+  }
+
+  async showAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'recruiterapp-98dfa',
+    appId: '1:63476690752:web:91e4731adb63e38a5cd4a5',
+    databaseURL: 'https://recruiterapp-98dfa-default-rtdb.firebaseio.com',
+    storageBucket: 'recruiterapp-98dfa.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyBbFHMZ3imKZDoN7wFi-SQa5geERjfuj3E',
+    authDomain: 'recruiterapp-98dfa.firebaseapp.com',
+    messagingSenderId: '63476690752',
+  },
   production: false
 };
 
